b2df959e3623748fac4caa065bdd35b9c58f72d7
Mohammed Sohail <sohailsameja@gmail.com>
2023-03-22 12:05:25 +0300

    site: update meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
  logo: (
     <>
-    <Image src="https://docs.grassecon.org/assets/favicon.svg" alt='CIC Stack' width={30} height={30} />
+    <Image src="/logo.svg" alt='CIC Stack' width={30} height={30} />
       <span style={{ marginLeft: '.4em' }}>
         CIC Stack
       </span>
@@ -18,8 +18,31 @@ const config: DocsThemeConfig = {
     link: 'https://discord.gg/xayVsrkHPQ',
   },
   docsRepositoryBase: 'https://github.com/grassrootseconomics/cic-stack-docs',
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  head: function Head() {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <meta name="msapplication-TileColor" content="#111111" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@grassecon" />
+        <meta name="twitter:creator" content="@grassecon" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="CIC Stack" />
+      </>
+    );
+  },  
   footer: {
-    text: 'CIC Stack',
+    text: 'Grassroots Economics CC BY-SA 4.0',
   },
   nextThemes: {
     defaultTheme: 'light'
